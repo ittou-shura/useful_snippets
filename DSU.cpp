b17916093f6ecf4be7267cb2f4373b90bf093f7c
ittou-shura <mukherjeerishiraj8@gmail.com>
2025-09-19 01:18:53 +0530

    added dsu

---
+++ DSU.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct dsu{
+    private:
+        vector<int> rank, parent;
+    public:
+        dsu(int n) : rank(n, 0), parent(n, 0){
+            for(int i = 0; i < n; i++) parent[i] = i;
+        }
+        int find_parent(int v) {
+            if(v == parent[v]) return v;
+            return parent[v] = find_parent(parent[v]);
+        }
+        void merge(int u, int v) {
+            int pu = find_parent(u);
+            int pv = find_parent(v);
+            if(rank[pu] < rank[pv])
+                swap(u, v);
+            parent[v] = u;
+            if(rank[pu] == rank[pv])
+                rank[pu]++;
+        }
+
+};
